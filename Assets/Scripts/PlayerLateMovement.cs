741764c19bb7b90385443a905bd9b2ef66c828f8
rummel1 <erencoker@hotmail.com.tr>
2023-12-09 22:39:29 +0300

    spawner

--- Assets/Scripts/PlayerLateMovement.cs
+++ Assets/Scripts/PlayerLateMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public class PlayLateMovement : MonoBehaviour
 {
-    public Transform target; // En öndeki karakterin Transform'u
+    public Transform target;
     public LineRenderer followerLineRenderer;
     public float forwardSpeed = 5f;
     public float lateralSpeed = 5f;
@@ -15,24 +15,23 @@ public class PlayLateMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Mevcut çizgileri temizle
+       
         followerLineRenderer.positionCount = 0;
 
-        // İz takip kodu buraya gelecek...
+       
         if (target != null)
         {
-            // En öndeki karakterin izini takip et
+            
             Vector3 currentPosition = transform.position;
             followerLineRenderer.positionCount++;
             followerLineRenderer.SetPosition(followerLineRenderer.positionCount - 1, currentPosition);
 
-            // Diğer karakterin pozisyonunu X ve Z ekseni üzerinde en öndeki karakterin izini takip ederek güncelle
-            float step = forwardSpeed * Time.deltaTime; // İleri yönlü hız ayarı
+          
+            float step = forwardSpeed * Time.deltaTime; 
             Vector3 targetPosition = new Vector3(target.position.x, transform.position.y, target.position.z);
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
-
-            // Sağa sola hareket
-            float horizontalStep = lateralSpeed * Time.deltaTime; // Sağa sola hız ayarı
+            
+            float horizontalStep = lateralSpeed * Time.deltaTime;
             float horizontalInput = Input.GetAxis("Horizontal");
             transform.Translate(Vector3.right * (horizontalInput * horizontalStep), Space.World);
         }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     private Vector3 whereischaracter;
     [SerializeField] private Transform _maincharacter;
-    public GameObject prefab; // Spawn edilecek karakterin prefab'ı
-    private bool hasPassedDoor = false; // Kapının içinden geçildiğini kontrol etmek için bir bayrak
+    public GameObject prefab; 
+    private bool hasPassedDoor = false; 
     private int x;
     private int y;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("ColorObject")) // Eğer kapı içine girdiyse
+        if (other.CompareTag("ColorObject")) 
         {
             hasPassedDoor = true;
         }
@@ -27,26 +26,18 @@ public class SpawnManager : MonoBehaviour
     {
         if (hasPassedDoor)
         {
-            // Kapının içinden geçildiyse ve her güncellemede yeni bir karakter spawn et
             SpawnCharacter();
-            hasPassedDoor = false; // Bayrağı sıfırla
+            hasPassedDoor = false; 
         }
     }
 
     void SpawnCharacter()
     {
-        while(x<500)
+        for (var i = 1; i < 2; i++)
         {
-            while (x<1)
-            {
-                x++;
-            }
-            GameObject newObject = Instantiate(prefab);
-            float newZ = _maincharacter.position.z;
-            newObject.transform.position = new Vector3(newObject.transform.position.x, newObject.transform.position.y,newZ*(-1*x));
-
-            x++;
-            break;
+            var position = _maincharacter.position;
+            float newZ = position.z +(i*-2.0f);
+            Instantiate(prefab, new Vector3(position.x, position.y, newZ), Quaternion.identity);
         }
     } 
 
