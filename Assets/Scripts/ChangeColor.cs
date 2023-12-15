97defb0088bdfe6e1297d96bd7f1abfbf2122a79
rummel1 <erencoker@hotmail.com.tr>
2023-12-15 19:04:14 +0300

    clone

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ChangeColor : MonoBehaviour
 {
@@ -7,36 +8,49 @@ public class ChangeColor : MonoBehaviour
     
     private bool colorChanged = false;
 
+    public GameObject clone;
     private void Start()
     {
         originalCharacterColor = GetComponent<Renderer>().material.color;
         currentCharacterColor = originalCharacterColor;
+        
     }
 
-    private void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("ColorObject"))
         {
             if (!colorChanged)
             {
-                Color newColor = other.GetComponent<MeshRenderer>().material.color;
+                Color newColor = other.GetComponent<Renderer>().material.color;
                 ChangeCharacterColor(newColor);
+                ChangeCloneColor(newColor);
+                
                 colorChanged = true;
             }
         }
     }
-
+    
     private void ChangeCharacterColor(Color newColor)
     {
-        GetComponent<MeshRenderer>().material.color = newColor;
+        GetComponent<Renderer>().material.color = newColor;
         currentCharacterColor = newColor;
+
+    }
+    private void ChangeCloneColor(Color newColor)
+    {
+        clone.GetComponent<Renderer>().material.color = newColor;
+
     }
     
+
     private void Update()
     {
+        
         if (colorChanged)
         {
             colorChanged = false;
         }
     }
+    
 }
